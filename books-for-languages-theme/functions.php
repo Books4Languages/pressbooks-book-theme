90b6348aa227bf27b457fa8a6b516ba38b978582
Zhitnitskii Daniil <danonchik98@gmail.com>
2018-05-15 15:14:03 +0200

    new theme initial files

---
+++ books-for-languages-theme/functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author  Pressbooks <code@pressbooks.com>
+ * @license GPLv2 (or any later version)
+ */
+
+add_action( 'after_setup_theme', function () {
+	add_theme_support('post-thumbnails');
+	add_theme_support( 'pressbooks_global_typography', 'grc' );
+} );
